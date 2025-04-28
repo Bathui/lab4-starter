47ceaf4ea1566067d1bc3e23be44559907bcbfe4
Bathui <michaelcui1126@gmail.com>
2025-04-27 23:12:28 -0700

    update

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -28,6 +28,13 @@ function discountPrices(prices, discount) {
     const discounted = []
     const length = prices.length;
     let discountedPrice = 0
+    
+    if (prices.length === 0) {
+        return false;
+    }
+    if (typeof discount !== 'number'|| !Array.isArray(prices)) {
+        return false;
+    }
     for(let i = 0; i < length; i++) {
         discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
